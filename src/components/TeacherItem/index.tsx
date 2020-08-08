6d9cefe7161c05336fe9970ba54ca58cc63be7fb
Ton <washingtonf.nascimento@gmail.com>
2020-08-07 23:36:18 -0400

    Finalização do form e integração com o backend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,38 +3,57 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
-function TeacherItem() {
+
+export interface Teacher {
+  id: number
+  subject:string
+  cost: number,
+  name: string,
+  avatar:string,
+  whatsapp: string,
+  bio: string
+} 
+
+export interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function newConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
           <header>
             <img 
-              src="https://avatars2.githubusercontent.com/u/66173308?s=460&u=4142881adc9439e68183e30eda5cef87fa8049f9&v=4" 
-              alt="Ton Cabral"/>
+              src={teacher.avatar} 
+              alt={teacher.name}/>
 
               <div>
                 <strong>
-                  Ton Cabral
+                  {teacher.name}
                 </strong>
-                <span>Química</span>
+                <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-              Entusiasta das melhores tecnologias de química avançada.
-              <br /> <br />
-              Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
               <p>
                 Preço/hora
-                <strong>80,00</strong>
+                <strong>{teacher.cost}</strong>
               </p>
-              <button>
+              <a target="_blank" onClick={newConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappIcon} alt="Whatsapp"/>
                 Entrar em contato
-              </button>
+              </a>
             </footer>
         </article>
   )
